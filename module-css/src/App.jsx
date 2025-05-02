33a67be6094aa574c082ecced1ab31b9f470241e
MorpheusByte <tahasaidcakmak@icloud.com>
2025-05-02 21:19:29 +0300

    CArd1

--- module-css/src/App.jsx
+++ module-css/src/App.jsx
@@ -1,14 +1,19 @@
 import Card from "./components/Card";
-import data from "./data"
-
+import data from "./data";
 
 function App() {
-  console.log(data)
+  console.log(data);
   return (
-<div>
-  <h1>LANGUAGES</h1>
-  <Card />
-</div>
+    <div>
+      <h1>LANGUAGES</h1>
+      {data.map((item) => (
+        <Card language={item.language} img={item.img} />
+      ))}
+
+      {/* <Card language={data[0].language} img={data[0].img} />
+      <Card language={data[1].language} img={data[1].img} />
+      <Card language={data[2].language} img={data[2].img} /> */}
+    </div>
   );
 }
 

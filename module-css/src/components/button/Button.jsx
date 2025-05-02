ff7b0eba6848a496a6b8a4c67de9708bd2aa19bb
MorpheusByte <tahasaidcakmak@icloud.com>
2025-05-02 21:52:27 +0300

    Button1

--- module-css/src/components/button/Button.jsx
+++ module-css/src/components/button/Button.jsx
@@ -1,10 +1,13 @@
-import './Button.css'
+// import './Button.css'
+import ButtonStyle from "./Button.module.css"
+
 
 const Button = () => {
     return(
-        <div className="wrapper">
-            <button className="btn btn-blue">İnfo</button>
-            <button className="btn btn-red">Back</button>
+        <div className={ButtonStyle.wrapper}>
+            <button className={`${ButtonStyle.btn} ${ButtonStyle["btn-blue"]}` }>İnfo</button>
+            <button className={`${ButtonStyle.btn} ${ButtonStyle["btn-red"]}` }>Back</button>
+            {/* <button className="btn btn-red">Back</button> */}
         </div>
     )
 }
